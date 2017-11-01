8d266a1479a8406d1e94303340071e6b20a5492e
biljana-zobenica <biljana.zobenica@outlook.com>
2017-11-01 21:40:07 +0100

    Basic SELECT query examples

---
+++ The SELECT Statement.sql
@@ -0,0 +1,23 @@
+-- This is a single-line comment
+/* This is for a multi-line comments */
+
+-- "Hello world" in SQL
+
+SELECT 'Hello Graeme';
+
+-- Retrieving all columns
+
+SELECT * FROM SalesLT.Product;
+
+-- Retrieving specific columns + using AS for aliases
+
+SELECT ProductID, Name, ListPrice, StandardCost AS Cost, ListPrice - StandardCost AS Margin, Color
+FROM  [SalesLT].[Product];
+
+/* AS can be omitted, but don't forget to put "," between statements */
+
+SELECT ProductID, Name, Color, Size, Color + Size AS Style
+FROM SalesLT.Product
+
+/* NULL means is a non-known value, not a blank cell.
+The Color + Size is possible because both columns consist text values. */
